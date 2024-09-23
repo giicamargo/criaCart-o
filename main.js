bc43218a4def5d43da506ea0985699746a5c013f
giicamargo <142258022+giicamargo@users.noreply.github.com>
2024-09-23 11:50:21 -0300

    referf

--- main.js
+++ main.js
@@ -3,4 +3,12 @@
     function soma(numero1, numero2){
         return numero1+numero2;
     }
-    console log(soma(1,2));
+    console.log(soma(1,2));
+    function areaQuadrado(lado){
+        return lado**2;
+    }
+    console.log(areaQuadrado(6));
+    let areatriangulo=function(base,altura){
+        return base*altura/2;
+    }
+    console.log(areatriangulo(3));
