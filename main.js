a12b1dd0ff050ed07e9d45f3d971045130606559
giicamargo <142258022+giicamargo@users.noreply.github.com>
2024-09-30 11:57:30 -0300

    gb

--- main.js
+++ main.js
@@ -23,7 +23,7 @@
 let estado=['Parana', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo',  'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
 let i=0,
 
-while(i<estado.lenght){
+while(i<estado.length){
     console.log(i);
-    console.log(estados[i]);
+    console.log(estado[i]);
 i++;}
