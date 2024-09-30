20ec877a07875c41a1788682c2aba4cb36f22bf7
giicamargo <142258022+giicamargo@users.noreply.github.com>
2024-09-30 11:21:00 -0300

    juhj

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-// let nomeUsuario='Giovanna';
-// alert (nomeUsuario) ;
-    function soma(numero1, numero2){
-        return numero1+numero2;
-    }
-    console.log(soma(1,2));
-    function areaQuadrado(lado){
-        return lado**2;
-    }
-    console.log(areaQuadrado(6));
-    let areatriangulo=function(base,altura){
-        return base*altura/2;
-    }
-    console.log(areatriangulo(3,4));
+// // let nomeUsuario='Giovanna';
+// // alert (nomeUsuario) ;
+//     function soma(numero1, numero2){
+//         return numero1+numero2;
+//     }
+//     console.log(soma(1,2));
+//     function areaQuadrado(lado){
+//         return lado**2;
+//     }
+//     console.log(areaQuadrado(6));
+//     let areatriangulo=function(base,altura){
+//         return base*altura/2;
+//     }
+//     console.log(areatriangulo(3,4));
 
-    let MedirMedia1=parseInt(prompt('nota primeiro trimestre'));
-    let MedirMedia2=parseInt(prompt('nota segundo trimestre'));
-    let MedirMedia3=parseInt(prompt('nota do terceiro trimestre'));
-    function media(num1,num2,num3){
-        return(num1,num2,num3)/3;
-    }
-    document.write(media(MedirMedia1,MedirMedia2,MedirMedia3));
+//     let MedirMedia1=parseInt(prompt('nota primeiro trimestre'));
+//     let MedirMedia2=parseInt(prompt('nota segundo trimestre'));
+//     let MedirMedia3=parseInt(prompt('nota do terceiro trimestre'));
+//     function media(num1,num2,num3){
+//         return(num1,num2,num3)/3;
+//     }
+//     document.write(media(MedirMedia1,MedirMedia2,MedirMedia3));
+let estado=['Parana', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo',  'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
